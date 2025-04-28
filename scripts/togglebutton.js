4dc9fe78abe6afdd41ae9c06f1fdde9f73d8a044
bonnieywang <149812277+bonnieywang@users.noreply.github.com>
2025-04-28 13:08:29 -0400

    added bone overlay button

--- scripts/togglebutton.js
+++ scripts/togglebutton.js
@@ -1,5 +1,23 @@
-function toggleOverlay() {
-    const overlay = document.getElementById('skinOverlay');
-    overlay.style.display = (overlay.style.display === 'none' || overlay.style.display === '') ? 'block' : 'none';
-  }
-  
+  function toggleSkinOverlay() {
+    const skinOverlay = document.getElementById('skinOverlay');
+    if (skinOverlay.style.display === 'none' || skinOverlay.style.display === '') {
+        skinOverlay.style.display = 'block';
+    } else {
+        skinOverlay.style.display = 'none';
+    }
+}
+
+function toggleBoneOverlay() {
+    const boneOverlayBottom = document.getElementById('boneBehind');
+    const boneOverlayTop = document.getElementById('boneFront');
+
+    const isHidden = (boneBehind.style.display === 'none' || boneFront.style.display === '');
+
+    if (isHidden) {
+        boneBehind.style.display = 'block';
+        boneFront.style.display = 'block';
+    } else {
+        boneBehind.style.display = 'none';
+        boneFront.style.display = 'none';
+    }
+}
